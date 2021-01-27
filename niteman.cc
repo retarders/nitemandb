dba4936fe0f5bc99a777159a953fd0a8d63ddb0f
pcranaway <pcranaway@tuta.io>
2021-01-27 10:21:03 +0200

    make use of CLI11

--- niteman.cc
+++ niteman.cc
@@ -1,7 +1,12 @@
-#include <iostream>
+#include "CLI/App.hpp"
+#include "CLI/Formatter.hpp"
+#include "CLI/Config.hpp"
 
-int main() {
-    std::cout << "nitemen arise\n";
+using namespace CLI;
 
+int main(int argc, char** argv) {
+    App app{"Starts the NitemanDB database"};
+
+    CLI11_PARSE(app, argc, argv);
     return 0;
 }
