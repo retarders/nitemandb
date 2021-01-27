07d7adc204f4f628a92677c48038996d4c9eea12
pcranaway <pcranaway@tuta.io>
2021-01-27 12:21:24 +0200

    add loading authentication section

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ class NitemanConfig {
         int port;
 
         /* authentication section */
-        bool authentication;
+        bool auth_enabled;
         string password;
 
 };
@@ -22,13 +22,22 @@ static NitemanConfig loadConfig(string path) {
     auto data = toml::parse(path);
 
     auto service = toml::find(data, "service");
+    auto authentication = toml::find(data, "authentication");
 
     string host = toml::find<string>(service, "host");
     int port = toml::find<int>(service, "port");
 
-
     config.host = host;
     config.port = port;
+
+    bool auth_enabled = toml::find<bool>(authentication, "enabled");
+    config.auth_enabled = auth_enabled;
+
+    if(auth_enabled) {
+        string password = toml::find<string>(authentication, "password");
+
+        config.password = password;
+    }
 
     return config;
 }
