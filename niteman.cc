69ca014d784920146735c4e3f416decdc4d0403c
pcranaway <pcranaway@tuta.io>
2021-01-27 10:40:34 +0200

    consume config option

--- niteman.cc
+++ niteman.cc
@@ -1,13 +1,16 @@
 #include "CLI/App.hpp"
 #include "CLI/Formatter.hpp"
 #include "CLI/Config.hpp"
+#include <string>
 
 using namespace CLI;
+using namespace std;
 
 int main(int argc, char** argv) {
     App app{"Starts the NitemanDB database"};
 
-    app.add_option("-c,--config", "niteman.toml", "Path to the config file");
+    string config_path = "niteman.toml";
+    app.add_option("-c,--config", config_path, "Path to the config file");
 
     CLI11_PARSE(app, argc, argv);
     return 0;
