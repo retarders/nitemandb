acadcbe442ebe3aaa8d97d9199a4de1cbf4c88a5
pcranaway <pcranaway@tuta.io>
2021-01-27 10:32:33 +0200

    add config option

--- niteman.cc
+++ niteman.cc
@@ -7,6 +7,8 @@ using namespace CLI;
 int main(int argc, char** argv) {
     App app{"Starts the NitemanDB database"};
 
+    app.add_option("-c,--config", "niteman.toml", "Path to the config file");
+
     CLI11_PARSE(app, argc, argv);
     return 0;
 }
