658dab1a710b5a01e1e1d100c1172735ff747f5e
pcranaway <pcranaway@tuta.io>
2021-01-28 11:54:42 +0200

    add data path option

--- src/niteman.cc
+++ src/niteman.cc
@@ -15,6 +15,9 @@ int main(int argc, char** argv) {
     string config_path = "niteman.toml";
     app.add_option("-c,--config", config_path, "Path to the config file");
 
+    string data_path = "/var/lib/niteman/";
+    app.add_option("-d,--data", data_path, "Path to the data directory");
+
     CLI11_PARSE(app, argc, argv);
 
     config = loadConfig(config_path);
